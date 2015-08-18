3b0243ede671077585b969caf1a1e2c207d33896
16jias38 <16jias38@millshighsch.org>
2015-08-18 09:19:45 -0700

    Hello

---
+++ HelloWorld2/HelloWorld.java
@@ -0,0 +1,14 @@
+
+/**
+ * My first program. Take two.
+ * 
+ * Sammy Jia
+ * Version 2.0 8/18/15
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Sammy Jia.");
+    }
+}
