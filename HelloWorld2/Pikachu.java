027f181be12b14e1df9825a7f8b0cc066286f04f
16jias38 <16jias38@millshighsch.org>
2015-08-25 09:07:10 -0700

    /hi

---
+++ HelloWorld2/Pikachu.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Pikachu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pikachu
+{
+ public static void main(String[] args)
+ {
+     System.out.println("Pikachu welcomes you to the world of pokemon.");
+     System.out.println("\t (\\__/)");
+     System.out.println("\t (o^.^)");
+     System.out.println("\tz(_(\")(\")");
+}
+}
---
+++ HelloWorld2/StarFigures.java
@@ -0,0 +1,40 @@
+
+/**
+ * StarFigures program done in class.
+ * 
+ * Sammy, Talita, Theodore, Kevin 
+ * Version 1.0 8/25/15
+ */
+public class StarFigures
+{
+   public static void main(String[] args)
+   {
+       printDouble();
+       printTriple();
+       System.out.println();
+       printDouble();
+       printTriple();
+       printDouble();
+       System.out.println();
+       printLine();
+       printDouble();
+       printTriple();
+    }
+    public static void printDouble()
+    {
+        System.out.println(" *****");
+        System.out.println(" *****");
+    }
+    public static void printTriple()
+    {
+        System.out.println("  * *");
+        System.out.println("   *");
+        System.out.println("  * *");
+    }
+    public static void printLine()
+    {
+        System.out.println("   *");
+        System.out.println("   *");
+        System.out.println("   *");
+    }
+    }
